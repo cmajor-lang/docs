da1f2c7702e954dfbf7b57fe3e713a0577fe0ab8
hazza-music <hrc.drury@btinternet.com>
2022-11-01 11:06:35 +0000

    Page Fix

---
+++ _includes/js/custom.js
@@ -0,0 +1,13 @@
+// jquerify.js
+// Add jQuery to any page that does not have it already.
+
+(function () {
+
+    if ( !window.jQuery ) {
+      var s = document.createElement('script');
+      s.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js');
+      document.body.appendChild(s);
+      console.log('jquery loaded!');
+    }
+  
+  })();
